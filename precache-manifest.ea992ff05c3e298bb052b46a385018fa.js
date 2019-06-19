c6e1b765077bbf682d3d52ad2b82fe2f71fc8204
Scripterix <piotr.adamkowski@gmail.com>
2019-06-19 12:56:14 +0200

    first commit

---
+++ precache-manifest.ea992ff05c3e298bb052b46a385018fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f314a3bd5b93b095fa40d420168dca5d",
+    "url": "/reacttodo/index.html"
+  },
+  {
+    "revision": "20d311dd2f182fd9e685",
+    "url": "/reacttodo/static/css/main.e31b6561.chunk.css"
+  },
+  {
+    "revision": "bfd9cc0500247bc197f5",
+    "url": "/reacttodo/static/js/2.45a652cd.chunk.js"
+  },
+  {
+    "revision": "20d311dd2f182fd9e685",
+    "url": "/reacttodo/static/js/main.e5cff655.chunk.js"
+  },
+  {
+    "revision": "89c463a4e4d8406ad55f",
+    "url": "/reacttodo/static/js/runtime~main.b56e3263.js"
+  }
+]);
